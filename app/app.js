af0cf360a1f3d8ee534b18b63b842f955ddb41e8
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2023-10-22 17:31:40 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,67 @@
+function initlisteners() {
+    $("#submit").click(function(e){
+        e.preventDefault();
+        let grass = $("#grass").val();
+        let fire = $("#fire").val();
+        let water = $("#water").val();
+        let normal = $("#normal").val();
+        let electric = $("#electric").val();
+        let fighting = $("#fighting").val();
+        let flying = $("#flying").val();
+        let ghost = $("#ghost").val();
+        let fairy = $("#fairy").val();
+        let dragon = $("#dragon").val();
+        let login = $("#login").val();
+        console.log("inputs " + grass + " " + fire 
+        + " " + water + " " + normal + " " + electric + " " + fighting + " " + flying + " " + ghost + " " + fairy + " " + dragon + " " + login);
+    });
+
+    $("#sub").click(function(e){
+        e.preventDefault();
+        let login = $("#login").val();
+        console.log("inputs " + login);
+    });
+
+    $("#sub2").click(function(e){
+        e.preventDefault();
+        let email = $("#email").val();
+        let password = $("#password").val();
+        let fName = $("#fName").val();
+        let lName = $("#lName").val();
+        console.log("inputs " + email + " " + password + " " + fName + " " + lName);
+    });
+
+    $("#edit").click(function(e){
+        e.preventDefault();
+
+        let userObj = {
+            grass: "Bellossom", 
+            fire: "Ceruledge",
+            water: "Vaporeon", 
+            normal: "Lopunny (Mega)",
+            electric: "Rotom",
+            fighting: "Gallade",
+            flying: "Gliscor",
+            ghost: "Mimikyu",
+            fairy: "Tinkaton",
+            dragon: "Garchomp",
+        };
+        console.log(userObj);
+       $("#grass").val(userObj.grass);
+       $("#fire").val(userObj.fire);
+       $("#water").val(userObj.water);
+       $("#normal").val(userObj.normal);
+       $("#electric").val(userObj.electric);
+       $("#fighting").val(userObj.fighting);
+       $("#flying").val(userObj.flying);
+       $("#ghost").val(userObj.ghost);
+       $("#fairy").val(userObj.fairy);
+       $("#dragon").val(userObj.dragon);
+        // console.log("inputs " + firstName + " " + lastName);
+
+    });
+}
+
+$(document).ready(function(){
+    initlisteners();
+});
